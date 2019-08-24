ad0b8199f4b553b6789495850aa074467f1249b2
KyeongsangNa <hellraris@gmail.com>
2019-08-24 17:48:04 +0900

    nuxt router 設定

--- front/pages/profile.vue
+++ front/pages/profile.vue
@@ -1,11 +1,19 @@
 <template>
-    
+  <div>
+    <div>profile page</div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  layout: "admin",
+  data() {
+    return {
+      name: "Nuxt.js"
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -1,11 +1,18 @@
 <template>
-    
+  <div>
+    <div>signup page</div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      name: 'Nuxt.js'
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
