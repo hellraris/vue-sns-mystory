c3a09108969c253468502914a72c3bb4e5238336
KyeongsangNa <hellraris@gmail.com>
2019-09-01 12:25:47 +0900

    loginのvalidation作成

--- front/components/LoginForm.vue
+++ front/components/LoginForm.vue
@@ -1,32 +1,41 @@
 <template>
-    <v-container>
-        <v-card>
-            <v-form>
-                <v-container>
-                    <v-text-field
-                      label="e-mail"
-                      type="email"
-                      required
-                    />
-                    <v-text-field
-                      label="password"
-                      type="password"
-                      required
-                    />
-                    <v-btn color="green" type="submit">login</v-btn>
-                    <v-btn nuxt to="/signup">singup</v-btn>
-                </v-container>
-            </v-form>
-        </v-card>
-    </v-container>   
+  <v-container>
+    <v-card>
+      <v-form ref="form" v-model="vaild" @submit.prevent="onSubmitForm">
+        <v-container>
+          <v-text-field label="e-mail" type="email" :rules="emailRules" required />
+          <v-text-field label="password" type="password" :rules="passwordRules" required />
+          <v-btn color="green" type="submit" :disabled="!vaild">login</v-btn>
+          <v-btn nuxt to="/signup">singup</v-btn>
+        </v-container>
+      </v-form>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
- export default {
-     
- }
+export default {
+  data() {
+    return {
+      vaild: false,
+      email: "",
+      password: "",
+      emailRules: [
+        v => !!v || "email is required",
+        v => /.+@.+/.test(v) || "email is not vaild"
+      ],
+      passwordRules: [
+        v => !!v || "password is required"
+      ]
+    };
+  },
+  methods: {
+      onSubmitForm() {
+          this.$refs.form.validate();
+      }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
