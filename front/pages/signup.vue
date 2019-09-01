9596a3917d495cd5f95fbb5b32f2ca41e7308177
KyeongsangNa <hellraris@gmail.com>
2019-09-01 12:17:55 +0900

    signupのvaildation作成

--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -4,29 +4,39 @@
       <v-card>
         <v-container>
           <v-subheader>signup</v-subheader>
-          <v-form>
+          <v-form ref="form" v-model="vaild" @submit.prevent="onSubmitForm">
             <v-text-field 
+              v-model="email"
               label="e-mail" 
               type="email" 
+              :rules="emailRules"
               required 
             />
             <v-text-field 
+              v-model="password"
               label="password" 
               type="password" 
+              :rules="passwordRules"
               required 
             />
             <v-text-field 
-              label="confirm password" 
+              v-model="passwordCheck"
+              label="passwordCheck" 
               type="password" 
+              :rules="passwordCheckRules"
               required 
             />
             <v-text-field 
+              v-model="nickname"
               label="nickname" 
-              type="nicknames" 
+              type="nickname" 
+              :rules="nicknameRules"              
               required 
             />
             <v-checkbox
+              v-model="terms"
               required
+              :rules="[v => !!v || 'please check this.']"
               label="welcome to mystory"
             />
             <v-btn color="green" type="submit">signup</v-btn>
@@ -41,8 +51,36 @@
 export default {
   data() {
     return {
-      name: "Nuxt.js"
+      vaild: false,
+      email: '',
+      password: '',
+      passwordCheck: '',
+      nickname: '',
+      terms: false,
+      emailRules: [
+        v => !!v || 'email is required',
+        v => /.+@.+/.test(v) || 'email is not vaild'
+      ],
+      nicknameRules: [
+        v => !!v || 'nickname is required',
+      ],
+      passwordRules: [
+        v => !!v || 'password is required',
+      ],
+      passwordCheckRules: [
+        v => !!v || 'passwordCheck is required',
+        v => v === this.password || 'Not equal to password'
+      ]
     };
+  },
+  methods: {
+    onSubmitForm() {
+      if (this.$refs.form.validate()) {
+        alert('submit success');
+      } else {
+        alert('form is not vaild');       
+      }
+    }
   }
 };
 </script>
