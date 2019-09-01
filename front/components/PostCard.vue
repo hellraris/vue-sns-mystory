f217de39c0c8002754be907f6b5fce0048cac9d9
KyeongsangNa <hellraris@gmail.com>
2019-09-01 11:05:38 +0900

    postComponentの作成

---
+++ front/components/PostCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card style="margin-bottom: 20px">
+    <v-image />
+    <v-card-text>
+      <div>
+        <h3>hellraris</h3>
+        <div>Good Morning</div>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text color="orange">
+        <v-icon>mdi-twitter-retweet</v-icon>
+      </v-btn>
+      <v-btn text color="orange">
+        <v-icon>mdi-heart-outline</v-icon>
+      </v-btn>
+      <v-btn text color="orange">
+        <v-icon>mdi-comment-outline</v-icon>
+      </v-btn>
+      <v-btn text color="orange">
+        <v-icon>mdi-dots-horizontal</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
