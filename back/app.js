d9ab6edb5c5ac3d9bdcb73b45c844a3953052c34
KyeongsangNa <hellraris@gmail.com>
2019-09-21 23:45:51 +0900

    route分割

--- back/app.js
+++ back/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 const passport = require('passport');
 const session = require('express-session');
 const cookie = require('cookie-parser');
@@ -8,6 +7,7 @@ const morgan = require('morgan');
 
 const db = require('./models');
 const passportConfig = require('./passport');
+const userRouter = require('./routes/user');
 const app = express();
 
 db.sequelize.sync();
@@ -37,66 +37,8 @@ app.get('/', (req, res) => {
   res.send('hello')
 });
 
-app.post('/user', async (req, res, next) => {
-  try {
-    const hash = await bcrypt.hash(req.body.password, 12);
-    const exUser = await db.User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
-    if (exUser) {
-      return res.status(403).json({
-        errorCode: 1,
-        message: 'already registered',
-      });
-    }
-    await db.User.create({
-      email: req.body.email,
-      password: hash,
-      nickname: req.body.nickname,
-    });
-    passport.authenticate('local', (err, user, info) => {
-      if (err) {
-        console.error(err);
-        return next(err);
-      }
-      if (info) {
-        return res.status(401).send(info.reason);
-      }
-      return req.login(user, async (err) => { // sessionにuser情報保存
-        if (err) {
-          console.error(err);
-          return next(err);
-        }
-        return res.json(user);
-      });
-    })(req, res, next);
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
-});
-
-app.post('/user/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      console.error(err);
-      return next(err);
-    }
-    if (info) {
-      return res.status(401).send(info.reason);
-    }
-    return req.login(user, async (err) => { // sessionにuser情報保存
-      if (err) {
-        console.error(err);
-        return next(err);
-      }
-      return res.json(user);
-    });
-  })(req, res, next);
-});
+app.use('/user', userRouter);
 
 app.listen(3085, () => {
   console.log(`server start! port ${3085}`);
-})
+});
---
+++ back/routes/user.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const db = require('../models');
+
+const router = express.Router();
+
+router.post('/');
+
+router.post('/', async (req, res, next) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 12);
+    const exUser = await db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (exUser) {
+      return res.status(403).json({
+        errorCode: 1,
+        message: 'already registered',
+      });
+    }
+    await db.User.create({
+      email: req.body.email,
+      password: hash,
+      nickname: req.body.nickname,
+    });
+    passport.authenticate('local', (err, user, info) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      if (info) {
+        return res.status(401).send(info.reason);
+      }
+      return req.login(user, async (err) => { // sessionにuser情報保存
+        if (err) {
+          console.error(err);
+          return next(err);
+        }
+        return res.json(user);
+      });
+    })(req, res, next);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async (err) => { // sessionにuser情報保存
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      return res.json(user);
+    });
+  })(req, res, next);
+});
+
+router.post('/logout', (req, res) => {
+  if (req.isAuthenticated()) {
+    req.logout();
+    req.session.destroy();
+    return res.status(200).send('ログアウト')
+  }
+});
+
+
+module.exports = router;
